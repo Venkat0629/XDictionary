5d6ced9b7dbb7876fb31679094e7db2e599156df
Venkat <venkatsaimane@gmail.com>
2024-02-03 13:40:43 +0530

    Added XDictionary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import XDictionary from "./Dictionary/Dictionary";
 
 function App() {
   return (
     <div className="App">
-   
+      <h1>Dictionary App</h1>
+      <XDictionary />
     </div>
   );
 }
---
+++ src/Dictionary/Dictionary.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import styles from "./Dictionary.module.css";
+
+export default function XDictionary() {
+  const [text, setText] = useState("");
+  const [meaning, setMeaning] = useState("");
+
+  const handleChange = (event) => setText(event.target.value);
+  const handleClick = () => fetchMeaning(text);
+  const dictionary = [
+    {
+      word: "React",
+      meaning: "A JavaScript library for building user interfaces.",
+    },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." },
+  ];
+
+  const fetchMeaning = (word) => {
+    try {
+      const meaning = dictionary.find(
+        (item) => item.word.toLowerCase() === word.toLowerCase()
+      );
+      if (meaning !== undefined) setMeaning(meaning.meaning);
+      else setMeaning("Word not found in the dictionary.");
+    } catch (error) {
+      console.error("Error fetching meaning of word:", error);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={handleChange}
+        placeholder="Search for a word..."
+      />
+      <button onClick={handleClick}>Search</button>
+      <div className={styles.wrapper}>
+        <strong> Definition: </strong>
+      </div>
+      {meaning && <p className={styles.text}>{meaning}</p>}
+    </div>
+  );
+}
